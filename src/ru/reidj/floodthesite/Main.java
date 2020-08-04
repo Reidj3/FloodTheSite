e09852ca37003e677629d42bedc83bb5ffaaebf6
Reidj3 <sp1.ke@mail.ru>
2020-08-04 16:51:53 +0300

    Теперь всё в одном классе

--- src/ru/reidj/floodthesite/Main.java
+++ src/ru/reidj/floodthesite/Main.java
@@ -1,8 +1,31 @@
 package ru.reidj.floodthesite;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Logic logic = new Logic();
-        logic.checkYourSelf();
+        int score = 0;
+        int moves;
+
+        while (score != 3) {
+            int num = (int) (Math.random() * 10);
+            int[] locationsCell = {num, num + 1, num + 2};
+
+            System.out.println("Сделайте ход");
+            Scanner scanner = new Scanner(System.in);
+            moves = scanner.nextInt();
+
+            for (int i : locationsCell) {
+                if (moves == i) {
+                    score++;
+                    System.out.println("Вы попали!");
+                    break;
+                }
+            }
+
+            if (score == locationsCell.length) {
+                System.out.println("Вы потопили vk.com :(");
+            }
+        }
     }
 }
