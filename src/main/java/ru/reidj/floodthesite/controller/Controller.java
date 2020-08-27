f977ba3645062c43a3372674b66e7215ce35f1ac
Reidj3 <sp1.ke@mail.ru>
2020-08-28 00:53:19 +0300

    Добавил перезапуск программы

--- src/main/java/ru/reidj/floodthesite/controller/Controller.java
+++ src/main/java/ru/reidj/floodthesite/controller/Controller.java
@@ -5,8 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import ru.reidj.floodthesite.Main;
 
-import java.util.*;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Controller {
@@ -20,13 +21,14 @@ public class Controller {
     private Button button;
 
     private int attempt = 0;
+    private Main main = new Main();
     private Random random = new Random();
     private ConcurrentHashMap<Integer, String> sites = new ConcurrentHashMap<Integer, String>() {{
-        put(random.nextInt(10), "VK.COM!");
-        put(random.nextInt(10), "TWITTER.COM!");
-        put(random.nextInt(10), "MAIL.RU!");
-        put(random.nextInt(10), "PIKABU.RU!");
-        put(random.nextInt(10), "REDDIT.COM!");
+        put(random.nextInt(12), "VK.COM!");
+        put(random.nextInt(13), "TWITTER.COM!");
+        put(random.nextInt(11), "MAIL.RU!");
+        put(random.nextInt(8), "PIKABU.RU!");
+        put(random.nextInt(9), "REDDIT.COM!");
     }};
 
     private Stage stage;
@@ -40,15 +42,24 @@ public class Controller {
         try {
             button.setOnAction(event -> {
                 attempt++;
-                    for (Integer keys : sites.keySet()) {
-                        if (textField.getText().contains(keys.toString())) {
-                            label.setText("Вы потопили " + sites.get(keys));
-                            System.out.println("" + sites.get(keys));
-                            sites.remove(keys);
-                        }
+                for (Integer keys : sites.keySet()) {
+                    if (textField.getText().contains(keys.toString())) {
+                        label.setText("Вы потопили " + sites.get(keys));
+                        System.out.println("" + sites.get(keys));
+                        sites.remove(keys);
                     }
-                    if (sites.isEmpty())
-                        label.setText("Игра окончена! \nВам потребовалось " + attempt + " хода(ов)");
+                }
+                if (sites.isEmpty()) {
+                    label.setText("Игра окончена! \nВам потребовалось " + attempt + " ходов");
+                    button.setText("Сыграть снова");
+                    button.setOnAction(restart -> {
+                        try {
+                            main.start(stage);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
             });
         } catch (Exception e) {
             e.printStackTrace();
